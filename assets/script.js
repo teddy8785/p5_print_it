902609e2dcd7e72fda657ab1b3e63afbab855477
teddy8587 <teddy.barieraud@gmail.com>
2024-05-25 13:47:40 +0200

    refactor loop bullets and function selectbullet

--- assets/script.js
+++ assets/script.js
@@ -20,68 +20,28 @@ const slides = [
 const arrowLeft = document.getElementById("arrow_left");
 const arrowRight = document.getElementById("arrow_right");
 let compteur = 0;
-// let i = 0;
+const bullets = [];
 
-// for (i = 0; i < slides.length; i++) {
-// 	bullet = document.createElement("span");
-// 	document.querySelector(".dots").appendChild(bullet);
-// 	(bullet).classList.add("dot");
-// 	selectBullet();
-// };
-
-// function selectBullet() {
-// 	if (i === compteur) {
-// 		(bullet).classList.add("dot_selected");
-// 	}
-// 	else {
-// 		(bullet).classList.remove("dot_selected");
-// 	}
-// }
-
-let bullet = document.createElement("span");
-let bullet2 = document.createElement("span");
-let bullet3 = document.createElement("span");
-let bullet4 = document.createElement("span");
-
-document.querySelector(".dots").appendChild(bullet);
-document.querySelector(".dots").appendChild(bullet2);
-document.querySelector(".dots").appendChild(bullet3);
-document.querySelector(".dots").appendChild(bullet4);
-(bullet).classList.add("dot");
-(bullet2).classList.add("dot");
-(bullet3).classList.add("dot");
-(bullet4).classList.add("dot");
-(bullet).classList.add("dot_selected");
+for (i = 0; i < slides.length; i++) {
+	let bullet = document.createElement("span");
+	document.querySelector(".dots").appendChild(bullet);
+	if (i === 0) {
+		bullet.classList.add("dot_selected");
+	}
+	bullet.classList.add("dot");
+	bullets.push(bullet);
+};
 
 function selectBullet() {
-	if (compteur === 0) {
-		(bullet).classList.add("dot_selected");
-		(bullet2).classList.remove("dot_selected");
-		(bullet3).classList.remove("dot_selected");
-		(bullet4).classList.remove("dot_selected");
-	}
-
-	else if (compteur === 1) {
-		(bullet).classList.remove("dot_selected");
-		(bullet2).classList.add("dot_selected");
-		(bullet3).classList.remove("dot_selected");
-		(bullet4).classList.remove("dot_selected");
-	}
-
-	else if (compteur === 2) {
-		(bullet).classList.remove("dot_selected");
-		(bullet2).classList.remove("dot_selected");
-		(bullet3).classList.add("dot_selected");
-		(bullet4).classList.remove("dot_selected");
-	}
-
-	else {
-		(bullet).classList.remove("dot_selected");
-		(bullet2).classList.remove("dot_selected");
-		(bullet3).classList.remove("dot_selected");
-		(bullet4).classList.add("dot_selected");
-	}
+    bullets.forEach((bullet, i) => {
+        if (i === compteur) {
+            bullet.classList.add("dot_selected");
+        } else {
+            bullet.classList.remove("dot_selected");
+        }
+    });
 }
+
 // 0 1 2 3 4
 arrowLeft.addEventListener("click", () => {
 	// si compteur egale 0 alors je dois mettre le compteur a 3
@@ -94,8 +54,6 @@ arrowLeft.addEventListener("click", () => {
 	document.getElementById("banner").firstElementChild.src = "./assets/images/slideshow/" + (slides[compteur].image);
 	document.querySelector("p").innerHTML = (slides[compteur].tagLine);
 	selectBullet();
-	// console.log(i);
-	// console.log(compteur);
 });
 
 arrowRight.addEventListener("click", () => {
@@ -108,6 +66,4 @@ arrowRight.addEventListener("click", () => {
 	document.getElementById("banner").firstElementChild.src = "./assets/images/slideshow/" + (slides[compteur].image);
 	document.querySelector("p").innerHTML = (slides[compteur].tagLine);
 	selectBullet();
-	// console.log(i);
-	// console.log(compteur);
 });
